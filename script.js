834ff4e241814372f00f2a00048b38771981ee71
agusTrih <ant3dong@gmail.com>
2020-06-19 10:14:37 +0700

    fix bug

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-Number.prototype.powerFive = function (n) {
-    let eksponen5 = Math.pow(n, 5);
+Number.prototype.powerFive = function () {
+    let num = this;
+    let eksponen5 = Math.pow(num, 5);
 
     return eksponen5;
 };
-
-let number = 5;
-console.log(number.powerFive(3));
+let number = 4;
+console.log(number.powerFive());
